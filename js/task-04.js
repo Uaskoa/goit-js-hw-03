9af8e46815327397c158b22412b8a7715ad3a060
Anna Shevchenko <uaskoa@gmail.com>
2020-12-10 21:55:02 +0200

    task-04

---
+++ js/task-04.js
@@ -0,0 +1,30 @@
+const countTotalSalary = function (employees) {
+  let salaries = Object.values(employees);
+  let total = 0;
+  for (let salary of salaries) {
+    total += salary;
+  }
+  return total;
+};
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+); // 400
+
+// Напиши функцию countTotalSalary(employees) принимающую объект зарплат.
+// Функция считает общую сумму запрплаты работников и возращает ее.
+// Каждое поле объекта, передаваемого в функцию, имеет вид "имя": "зарплата".
