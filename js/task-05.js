a9c732ef6e0a32999a4830bd4c729596c4c61c3f
Anna Shevchenko <uaskoa@gmail.com>
2020-12-11 23:47:19 +0200

    task-05

--- js/task-05.js
+++ js/task-05.js
@@ -1,28 +1,31 @@
-// const products = [
-//   { name: 'Радар', price: 1300, quantity: 4 },
-//   { name: 'Сканер', price: 2700, quantity: 3 },
-//   { name: 'Дроид', price: 400, quantity: 7 },
-//   { name: 'Захват', price: 1200, quantity: 2 },
-// ];
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
 
-// const getAllPropValues = function(arr, prop) {
-//   // твой код
-// };
+// console.log(products);
+const getAllPropValues = function (arr, prop) {
+  let array = [];
 
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+  for (const product of arr) {
+    if (product[prop]) {
+      array.push(product[prop]);
+    }
+  }
+  return array;
+};
 
-// console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
 
-// console.log(getAllPropValues(products, 'category')); // []
+console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
 
+console.log(getAllPropValues(products, "category")); // []
 
-// // Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов
-// // и имя свойства.Возвращает массивзначений определенного свойства prop из
-// // каждого объекта в массиве.
-
-
-
-
+// Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов
+// и имя свойства.Возвращает массивзначений определенного свойства prop из
+// каждого объекта в массиве.
--- js/task-06.js
+++ js/task-06.js
@@ -3,12 +3,12 @@
 
 
 
-// const products = [
-//   { name: 'Радар', price: 1300, quantity: 4 },
-//   { name: 'Сканер', price: 2700, quantity: 3 },
-//   { name: 'Дроид', price: 400, quantity: 7 },
-//   { name: 'Захват', price: 1200, quantity: 2 },
-// ];
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
 
 // const calculateTotalPrice = function(allProdcuts, productName) {
 //   // твой код
